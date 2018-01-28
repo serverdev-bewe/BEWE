e73e067bff6b267380a3b5550953ddd45d2d6084
soyoung <3457soso@naver.com>
2018-01-28 22:35:10 +0900

    sh 파일 추가

---
+++ start.sh
@@ -0,0 +1,10 @@
+sudo systemctl start redis
+cd BACKEND/AuthApiServer
+npm install
+npm start
+cd ../PlatformApiServer
+npm install
+npm start
+cd ../../FRONTEND/ReactReduxVer_1
+npm install
+npm start
