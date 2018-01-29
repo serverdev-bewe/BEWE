2c7b29e5a7408c888e30aa9de5411a9bc4e4dad5
manhyuk <rnanhyuk@gmail.com>
2018-01-29 14:32:45 +0900

    Store 구매 / 구매기록 조회 추가

--- BACKEND/StoreApiServer/routes/api.js
+++ BACKEND/StoreApiServer/routes/api.js
@@ -3,12 +3,15 @@
 
 const imageCtrl = require('../controllers/ImageCtrl');
 const authCtrl = require('../controllers/AuthCtrl');
+const storeCtrl = require('../controllers/StoreCtrl');
 
 
 
 module.exports = (router) => {
 
-
+  router.route('/store')
+    .get(authCtrl.auth, storeCtrl.list)
+    .post(authCtrl.auth, storeCtrl.purchase);
 
   return router;
 };
