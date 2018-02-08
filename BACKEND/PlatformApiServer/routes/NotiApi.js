9f70e89e16550a94b3db06a7aba98ed2139f50fb
Soyoung <3457soso@naver.com>
2018-02-08 17:17:56 +0900

    유저 프로필 게임 앞단 하던 중...

--- BACKEND/PlatformApiServer/routes/NotiApi.js
+++ BACKEND/PlatformApiServer/routes/NotiApi.js
@@ -1,5 +1,3 @@
-// import { auth } from '../controllers/AuthCtrl';
-
 const validate = require('express-validation');
 
 const authCtrl = require('../controllers/AuthCtrl');
@@ -9,7 +7,7 @@ const notiCtrl = require('../controllers/NotiCtrl');
 module.exports = (router) => {
 
   router.route('/users/noti')
-    .get(authCtrl.auth, authCtrl.auth, notiCtrl.list);
+    .get(authCtrl.auth, notiCtrl.list);
 
   router.route('/users/noti/:idx')
     .get(authCtrl.auth, notiCtrl.check);
