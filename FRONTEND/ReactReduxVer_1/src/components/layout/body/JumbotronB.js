cb76a337b8e45444ae3c9a5fc7a9f4a1136d9265
Jackson Lee <1ilsang@naver.com>
2018-02-16 06:42:44 +0900

    footer 설정완료 및 smilegateLogo.png추가

--- FRONTEND/ReactReduxVer_1/src/components/layout/body/JumbotronB.js
+++ FRONTEND/ReactReduxVer_1/src/components/layout/body/JumbotronB.js
@@ -6,6 +6,7 @@ class JumbotronB extends Component{
   return (
     <div>
       <Jumbotron>
+      
         <h1 className="display-3">Hello, world!</h1>
         <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
         <hr className="my-2" />
--- FRONTEND/ReactReduxVer_1/src/components/layout/body/VideoPlayer.js
+++ FRONTEND/ReactReduxVer_1/src/components/layout/body/VideoPlayer.js
@@ -4,12 +4,12 @@ import Slider from './Slider';
 
 export default (props) => {
   return (
-    <div style={{height:"1000px"}}>
+    <div style={{height:"880px"}}>
     <div>
     <iframe 
       width="100%" 
       height="100%" 
-      src="https://www.youtube.com/embed/Lj6Y6JCu-l4?rel=0&autoplay=1&loop=1&playlist=Lj6Y6JCu-l4&showinfo=0&mute=1&fs=0"
+      src="https://www.youtube.com/embed/Lj6Y6JCu-l4?rel=0&autoplay=1&loop=1&playlist=Lj6Y6JCu-l4&showinfo=0&mute=1&fs=0&controls=0"
       scrolling="no"
       frameBorder="0"
       allowFullScreen
