ea158a44c5f0ebb7061155593ce3e5e77f2d08de
ManHyuk <manhyuk@kw.ac.kr>
2018-02-17 03:51:45 +0900

    FRONT / cms 설정

--- FRONTEND/ReactReduxVer_1/src/actions/CMS/CMSAction.js
+++ FRONTEND/ReactReduxVer_1/src/actions/CMS/CMSAction.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 export const FETCH_CONTENTS = 'FETCH_CONTENTS';
 export const FETCH_POST = 'FETCH_POST';
+export const FETCH_POST_DETAIL = 'FETCH_POST_DETAIL';
 
 const ROOT_URL = 'http://127.0.0.1:3003/api/cms';
 
@@ -17,6 +18,17 @@ exports.fetchContents = () => {
 
   return {
     type: FETCH_CONTENTS,
+    payload: request
+  }
+};
+
+exports.fetchContentsDetail = (idx) => {
+  const request = axios.get(`${ROOT_URL}/${idx}`, {
+    headers: JSON.parse(localStorage.getItem('token'))
+  });
+
+  return {
+    type: FETCH_POST_DETAIL,
     payload: request
   }
 };
