6ff2afe47d804cf56a84b8b388507790e870572a
soyoung <3457soso@naver.com>
2018-02-18 23:34:04 +0900

    Platform 리팩토링

--- COMMON/Auth/AuthCtrl.js
+++ COMMON/Auth/AuthCtrl.js
@@ -9,7 +9,8 @@ let authCtrl = {};
 
 exports.setup = (pool, config, redis, jwt) => {
   const authModel = authModelSetup.setup(pool, config, redis, jwt);
-
+  authCtrl.authModel = authModel;
+  
   authCtrl.auth = (req, res, next) => {
     if (!req.headers.token) {
       return next(401);
